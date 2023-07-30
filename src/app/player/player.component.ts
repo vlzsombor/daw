fa1ec1c982632972e533fb968e1aeb8f38a0469a
Zsombor Veres-Lakos <zsombor.veres-lakos@incadea.com>
2023-07-30 18:44:04 +0200

    feat: note playing

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -15,97 +15,57 @@ import { Pianoroll } from 'webaudio-pianoroll';
 })
 export class PlayerComponent {
 
-  private iteration: number = 0;
   public static LENGTH = 16;
   @ViewChild('myname') input;
 
   private scale = [
-    "E6",
-    "D6",
-    "B5",
-    "A5",
-    "G5",
-    "E5",
-    "D5",
-    "B4",
-    "A4",
-    "G4",
-    "E4",
-    "D4",
-    "B3",
-    "A3",
+    "E3",
     "G3",
-    "E3"
+    "A3",
+    "B3",
+    "D4",
+    "E4",
+    "G4",
+    "A4",
+    "B4",
+    "D5",
+    "E5",
+    "G5",
+    "A5",
+    "B5",
+    "D6",
+    "E6",
   ];
 
 
-  constructor(private instrument: InstrumentService, @Inject(DOCUMENT) private document: Document) {
-
-    for (var i: number = 0; i < PlayerComponent.LENGTH; i++) {
-      this.midiButtons[i] = [];
-      for (var j: number = 0; j < PlayerComponent.LENGTH; j++) {
-        this.midiButtons[i][j] = {
-          row: i,
-          column: j
-        };
-      }
-    }
-
-  }
-
-  ngOnInit() {
-    // setInterval(() => { this.tick() }, 0.15 * 1000);
-  }
-  tick() {
-
-    console.log('hello')
-
-    this.midiButtons.map(d => d[this.iteration % PlayerComponent.LENGTH]).forEach(column => {
-      let saw = new Wad({ source: 'triangle', env: { decay: 0.25, sustain: 0, release: 0.5 } });
-
-      if (column.enabled) {
-        saw.play({ pitch: this.scale[column.row], label: 'A4' });
-      }
-
-    });
-    this.iteration++;
+  constructor(@Inject(DOCUMENT) private document: Document) {
   }
 
 
   handleButtonClick() {
-    // This function will be called when the button is clicked
     console.log('Button clicked!');
-    // Add your desired logic here
     let saw = new Wad({ source: 'sine', env: { decay: 0.25, sustain: 0, release: 0.5 } });
-
     saw.play({ pitch: '440', label: 'A4' });
-
   }
 
-  protected midiButtons: MidiButton[][] = [];
 
-
-  /*
-  
-  play() function passes necessary data for playing to callback function like : callback({t:noteOnTime, g:noteOffTime, n:noteNumber})
-  */
   timebase = 480;
 
   Callback(ev: { t: number, g: number, n: number }) {
 
+    // 52 - 88
+    //52 = 0
+    // 88 = 16
+    // 57 -> A4 440hz
     console.log('Button clicked!');
     // Add your desired logic here
-    let saw = new Wad({ source: 'sine', env: { decay: 0.25, sustain: 0, release: 0.5 } });
+    let saw = new Wad({ source: 'sine', env: { decay: 0.30, sustain: 0, release: 0.5 } });
 
-    saw.play({ pitch: '880', label: 'A4' });
-
-
+let a = this.scale;
+    saw.play({ pitch: this.scale[ev.n], label: 'A4' });
   }
   Play() {
-    console.log('hellofasdasfsxd');
-    // let audioPlayer = this.document.getElementById("proll") as Pianoroll
-
-    this.input.nativeElement.play(Wad.audioContext, this.Callback);
+    this.input.nativeElement.play(Wad.audioContext, this.Callback.bind(this));
   }
 
   Layout(k) {
